b3a198a73715359dd5c2f4a20da5a711e4b5d76f
David Bašić <d.basic97.db@gmail.com>
2022-10-11 00:19:01 +0200

    login form created, closing logic done

--- SQLManagerApp/MainForm.cs
+++ SQLManagerApp/MainForm.cs
@@ -10,11 +10,18 @@ using System.Windows.Forms;
 
 namespace SQLManagerApp
 {
-    public partial class Form1 : Form
+    public partial class MainForm : Form
     {
-        public Form1()
+        public MainForm()
         {
             InitializeComponent();
         }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void MainForm_FormClosed(object sender, FormClosedEventArgs e) => Application.Exit();
     }
 }
